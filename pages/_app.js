a8ca597d4c4d651f315a1038731d21a9cd886cfa
Jateen <jateen.joshi111@gmail.com>
2025-06-28 17:36:51 +0530

    Build final portfolio version

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import "@/styles/globals.css";
+// File: pages/_app.js
+import '../styles/globals.css';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return <Component {...pageProps} />;
 }
